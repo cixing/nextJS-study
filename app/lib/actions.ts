dc5f665975cb6c491f15435f49443532ad4cef46
zhangminghua02 <zhangminghua02@meituan.com>
2026-04-09 14:07:12 +0800

    添加登录和鉴权能力

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,8 @@ import { z } from 'zod';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -85,3 +87,23 @@ export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
